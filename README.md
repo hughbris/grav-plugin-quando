a45e9dae349a83fa3ec75a593360ac3f4f53b95b
Hugh Barnes <dev@hubns.com>
2018-06-06 21:25:14 +1200

    Initial, untethered for github release

---
+++ README.md
@@ -0,0 +1,63 @@
+# Opaque Plugin
+
+The **Opaque** Plugin is for [Grav CMS](http://github.com/getgrav/grav).
+
+It exposes business opening hours and other service hours to Grav as a Twig hash. It provides starter templates you might want to copy into your theme, customise, and include.
+
+## Installation
+
+Installing the Opaque plugin can be done in one of two ways. The GPM (Grav Package Manager) installation method enables you to quickly and easily install the plugin with a simple terminal command, while the manual method enables you to do so via a zip file.
+
+### GPM Installation (Preferred)
+
+The simplest way to install this plugin is via the [Grav Package Manager (GPM)](http://learn.getgrav.org/advanced/grav-gpm) through your system's terminal (also called the command line).  From the root of your Grav install type:
+
+    bin/gpm install opaque
+
+This will install the Opaque plugin into your `/user/plugins` directory within Grav. Its files can be found under `/your/site/grav/user/plugins/opaque`.
+
+### Manual Installation
+
+To install this plugin, just download the zip version of this repository and unzip it under `/your/site/grav/user/plugins`. Then, rename the folder to `opaque`. You can find these files on [GitHub](https://github.com/hughbris/grav-plugin-opaque) or via [GetGrav.org](http://getgrav.org/downloads/plugins#extras).
+
+You should now have all the plugin files under
+
+    /your/site/grav/user/plugins/opaque
+
+> NOTE: This plugin is a modular component for Grav which requires [Grav](http://github.com/getgrav/grav) and the [Error](https://github.com/getgrav/grav-plugin-error) and [Problems](https://github.com/getgrav/grav-plugin-problems) to operate.
+
+## Configuration
+
+Before configuring this plugin, you should copy the `user/plugins/opaque/opaque.yaml` to `user/config/plugins/opaque.yaml` and only edit that copy.
+
+Here is the default configuration and an explanation of available options:
+
+```yaml
+enabled: true
+hours: [] # When populated, an indexed array of service hours listings, a complex YAML structure best configured through the Admin dashboard (when the blueprint is done!). One of these listings should be called 'opening' if you want any of the templates to work out of the box.
+```
+
+## Usage
+
+It's enabled by default on install but will do nothing unless you use it.
+
+Configure the hours through the Admin plugin is probably simplest. Or copy the [sample YAML](data.sample.yaml) (in fact, you'll have to do that until I add a blueprint for hours).
+
+Copy, customise, and include one of the sample templates, probably start with a simple partial:
+
+* [This week's hours](templates/partial/panel_hours.html.twig) web panel
+* [Regular schedule](templates/partial/hours.html.twig)
+* [Exceptions list](templates/partial/hours_exceptions.html.twig)
+
+Examples in the wild:
+
+* [QE2 Dental opening hours](https://qe2dental.co.nz/about/opening) page
+
+## Credits
+
+I had this in my original composer.json for some reason: https://github.com/spatie/opening-hours. I'm sorry, I can't remember, but it looks like I have been inspired heavily by this, its API especially.
+
+## To Do
+
+- [ ] It's all supposed to be in the [issues](issues).
+
---
+++ opaque.php
@@ -0,0 +1,356 @@
+<?php
+namespace Grav\Plugin;
+
+use Grav\Common\Plugin;
+use RocketTheme\Toolbox\Event\Event;
+
+/**
+ * Class OpaquePlugin
+ * @package Grav\Plugin
+ */
+class OpaquePlugin extends Plugin
+{
+    /**
+     * @return array
+     *
+     * The getSubscribedEvents() gives the core a list of events
+     *     that the plugin wants to listen to. The key of each
+     *     array section is the event that the plugin listens to
+     *     and the value (in the form of an array) contains the
+     *     callable (or function) as well as the priority. The
+     *     higher the number the higher the priority.
+     */
+    public static function getSubscribedEvents() {
+        return [
+            'onPluginsInitialized' => ['initializeIfRequired', 0],
+        ];
+    }
+
+    /**
+     * Initialize the plugin
+     */
+    public function initializeIfRequired() {
+        // Don't proceed if we are in the admin plugin
+        if ($this->isAdmin()) {
+            return;
+        }
+
+        // Enable the main event we are interested in
+        $this->enable([
+            'onTwigSiteVariables' => ['initializePlugin', 0],
+            'onTwigTemplatePaths' => ['addTwigTemplatePaths', 0],
+            'onTwigExtensions' => ['addTwigExtensions', 0],
+        ]);
+    }
+
+    public function addTwigExtensions() {
+        // require_once(__DIR__ . '/twig/ExampleTwigExtension.php');
+        $this->grav['twig']->twig->addExtension(new DatetimeFormatExtension());
+        $this->grav['twig']->twig->addExtension(new TranslateExtension());
+    }
+
+	/**
+	* Add current directory to twig lookup paths.
+	*/
+	public function addTwigTemplatePaths()
+	{
+		$this->grav['twig']->twig_paths[] = __DIR__ . '/templates';
+	}
+
+    public function initializePlugin() {
+		// NB: $services_times vs. $service_times !!
+		$services_times = $this->config['plugins']['opaque']['hours'];
+
+		$openhrs = [];
+		foreach($services_times as $name => $service_times) {
+			$openhrs[$name] = new Opnhrs($service_times);
+		}
+
+		$this->grav['twig']->twig_vars['openhrs'] = $openhrs;
+	}
+
+}
+
+class DatetimeFormatExtension extends \Twig_Extension {
+
+    public function getName() {
+        return 'DatetimeFormatExtension';
+    }
+
+    public function getFilters() {
+        return [
+            new \Twig_SimpleFilter('briefTime', 'Grav\Plugin\Opnhrs::briefTime'),
+        ];
+    }
+
+}
+
+class TranslateExtension extends \Twig_Extension {
+
+    public function getName() {
+        return 'TranslateExtension';
+    }
+
+    public function getFilters() {
+        return [
+            new \Twig_SimpleFilter('tav', [$this, 'translateArrayUsingValue']),
+        ];
+    }
+
+	public function translateArrayUsingValue($key, $term, $lang=NULL, $case_sensitive= FALSE, $default=NULL) {
+		global $grav;
+
+		if (is_null($lang)) {
+			$lang = $grav['language']->getDefault();
+		}
+		$vocab = $grav['language']->getTranslation($lang, $key, TRUE);
+
+		if (!$case_sensitive) {
+			$term = strtolower($term);
+			$vocab = array_map('strtolower', $vocab);
+		}
+
+		$position = array_search($term, $vocab);
+
+		if ($position === FALSE) {
+			return $default;
+		}
+		return $grav['language']->translateArray($key, $position);
+	}
+
+}
+
+class Opnhrs {
+	private $schedule;
+	const DOW = [
+		'sunday'    => 0,
+		'monday'    => 1,
+		'tuesday'   => 2,
+		'wednesday' => 3,
+		'thursday'  => 4,
+		'friday'    => 5,
+		'saturday'  => 6,
+		];
+
+	function __construct($service) {
+		$this->load($service);
+		return $this;
+	}
+
+	private function load($service) {
+		$this->schedule = $service;
+		$this->timezone = new \DateTimeZone($this->schedule['timezone']);
+		// TODO - validate the times ??
+	}
+
+	public function opensOn($day_name, $schedule=NULL) {
+		if (is_null($schedule)) {
+			$schedule = $this->schedule['regular'];
+		}
+		return ( array_key_exists($day_name, $schedule) AND !empty($schedule[$day_name]) );
+	}
+
+	public function hoursOn($day_name, $schedule=NULL) {
+		if (is_null($schedule)) {
+			$schedule = $this->schedule['regular'];
+		}
+
+		$ret = [];
+
+		if ($this->opensOn($day_name, $schedule)) {
+			$ret = $schedule[$day_name];
+		}
+
+		return $ret;
+	}
+
+	private function opensLaterOnDay($dto, $hours) { // "beforeCOB" ??
+		if (empty($hours)) {
+			return FALSE;
+		}
+		else {
+			$timeOfDay = $dto->format('H:i');
+			$last_opening = array_pop($hours);
+			return ( $last_opening['shuts'] > $timeOfDay );
+		}
+	}
+
+    public static function briefTime($timeOfDay, $pattern='g.ia', $truncateZeroComponents=['.i']) {
+		$to = \DateTime::createFromFormat('Y-m-d H:i', "1970-01-01 $timeOfDay"); // $to is "time object" (no significant date component)
+		$mod_pattern = $pattern;
+		foreach ($truncateZeroComponents as $truncateableComponent) {
+			if (intval(strtr($to->format($truncateableComponent),'.-','  '), 10) == 0) {
+				$mod_pattern = str_replace($truncateableComponent, '', $mod_pattern);
+			}
+		}
+		return $to->format($mod_pattern);
+    }
+
+	public function formatSchedule($schedule, $pattern, $truncateZeroComponents=[]) {
+		$ret = [];
+		foreach ($schedule as $opening) {
+			foreach ($opening as $activity => $timeOfDay) {
+				$opening[$activity] = $this->briefTime($timeOfDay, $pattern, $truncateZeroComponents);
+			}
+			$ret[] = $opening;
+		}
+		return $ret;
+	}
+
+	public function statusAt($dto, $includeNext=TRUE) {
+		$ret = [];
+		$dto->setTimezone($this->timezone);
+
+		$ret['hours'] = $this->scheduleAt($dto);
+		$ret['open'] = $this->withinHours($dto, $ret['hours']);
+		$ret['open_on_day'] = !empty($ret['hours']); // this isn't worth a function - "tradingDay" ??
+		$ret['open_later_on_day'] = $this->opensLaterOnDay($dto, $ret['hours']);
+		// $ret['debug'] = [$dto, $ret['hours']];
+		// $ret['testsched'] = [['opens'=>'00:10','shuts'=>'18:00'],['opens'=>'19:20','shuts'=>'21:00']];
+		// $ret['debug'] = $this->formatSchedule($ret['testsched'], 'g.ia', ['.i']);
+		// $ret['debug'] = $this->schedulesAfter($dto, 2);
+		// $ret['debug'] = $this->schedulesWeek($dto, -1);
+		if ($includeNext) {
+			$ret['until'] = $this->nextChange($dto, $ret['hours']);
+		}
+
+		return $ret;
+	}
+
+	public function openAt($dto) {
+		return $this->statusAt($dto, FALSE)['open'];
+	}
+
+	public function isOpen() {
+		$dto = new \DateTime();
+		return $this->openAt($dto);
+	}
+
+	private function nextChange($dto, $hours) {
+		$timeOfDay = $dto->format('H:i');
+
+		foreach($hours as $seq => $opening) {
+
+			// test for easy case where $timeOfDay is before the first open
+			if ( $seq == 0 AND $timeOfDay < $opening['opens'] ) {
+				return \DateTime::createFromFormat('Y-m-d H:i', $dto->format('Y-m-d ') . $opening['opens']);
+			}
+
+			if ( $opening['opens'] <= $timeOfDay AND $opening['shuts'] > $timeOfDay ) {
+				return \DateTime::createFromFormat('Y-m-d H:i', $dto->format('Y-m-d ') . $opening['shuts']);
+				// TODO: handle times going over midnight here, not needed now
+			}
+		}
+
+		// still here? let's try the next day until we get something (recursive calls)
+		$nextDay = $dto->add(new \DateInterval('P1D'));
+		$nextDayHours = $this->scheduleAt($nextDay);
+		$nextDay = \DateTime::createFromFormat('Y-m-d H:i', $nextDay->format('Y-m-d') . ' 00:00'); // bring it back to midnight
+		return $this->nextChange($nextDay, $nextDayHours);
+
+	}
+
+	private function withinHours($dto, $hours) {
+		$timeOfDay = $dto->format('H:i');
+		foreach($hours as $opening) {
+			if ( $opening['opens'] <= $timeOfDay AND $opening['shuts'] > $timeOfDay ) {
+				return TRUE;
+			}
+		}
+		return FALSE;
+	}
+
+	private function dayStringFromNumber($day_number) {
+		return array_search($day_number, self::DOW);
+	}
+
+	private function dayStringFromDateTime($dto) {
+		return $this->dayStringFromNumber($dto->format('w'));
+	}
+
+	private function scheduleAt($dto) {
+
+		$date_ymd = $dto->format('Y-m-d');
+		$day_name = $this->dayStringFromDateTime($dto);
+
+		if ($day_name) {
+
+			// look for period matches
+			// NB - we are taking first match from periods array, so that's the precedence
+			foreach($this->schedule['periods'] as $period) {
+				if ( $period['start'] <= $date_ymd AND $period['finish'] >= $date_ymd ) {
+					return $this->findInSchedule($day_name, $date_ymd, $period);
+				}
+			}
+
+			// look for global matches
+			return $this->findInSchedule($day_name, $date_ymd, $this->schedule);
+		}
+
+		return [];
+	}
+
+	/*
+	public function schedulesBetween($start, $finish) { // TODO: stub, make this wrap schedulesAfter()
+	}
+	*/
+
+	public function schedulesAfter($start_dto, $days_duration) {
+		$ret = []; $day = [];
+		for ($i=0; $i < $days_duration ; $i++) {
+			$day[] = ( $i == 0 ? clone $start_dto : clone $day[$i-1] );
+			if ($i > 0) {
+				$day[$i]->add(new \DateInterval('P1D')); // thanks PHP, WTF were you thinking when you created this class API ??
+			}
+			$ret[] = ['day' => $day[$i], 'schedule' => $this->scheduleAt($day[$i])];
+		}
+		return $ret;
+	}
+
+	public function regularSchedule() {
+		return $this->schedule['regular'];
+		/*
+		$ret = [];
+		foreach (array_keys(self::DOW) as $day_name) {
+			$ret[] = $this->hoursOn($day_name);
+		}
+
+		return $ret;
+		*/
+	}
+
+	public function getSchedule($member=NULL) {
+		if (is_null($member)) {
+			return $this->schedule;
+		}
+		elseif (!array_key_exists($member, $this->schedule)) {
+			return [];
+		}
+		else {
+			return($this->schedule[$member]);
+		}
+	}
+
+	public function schedulesWeek($start_dto, $days_context=NULL) {
+		$day = clone $start_dto;
+		if ($days_context) {
+			$date_method = ( $days_context < 0 ? 'sub' : 'add' );
+			call_user_func( [$day, $date_method], new \DateInterval('P' . abs($days_context) . 'D'));
+		}
+		return $this->schedulesAfter($day, 7);
+	}
+
+	private function findInSchedule($day_name, $date_ymd, $schedule) {
+
+		// look for exception matches
+		if (array_key_exists('exceptions', $schedule)) {
+			if(array_key_exists($date_ymd, $schedule['exceptions'])) {
+				return $schedule['exceptions'][$date_ymd]['hours'];
+			}
+		}
+
+		// look for regular day matches
+		return $this->hoursOn($day_name, $schedule['regular']);
+	}
+
+}
